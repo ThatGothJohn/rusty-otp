1263c86daa17326a521820d7a49f82876184a447
ThatGuyJohnForbes <thatbotjohn@gmail.com>
2021-03-24 15:27:06 +0000

    Getting to grips with crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Write, stdout};
+
+use crossterm::{execute, ExecutableCommand, cursor, Result};
+
+
+fn main() -> Result<()>{
+    let mut stdout = stdout();
+    println!("dirt");
+    execute!(stdout, cursor::MoveUp(1))?;
+    println!("Bang!, and the dirt is gone.");
+    Ok(())
 }
