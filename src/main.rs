a7ec9b4c4c8a487c6b0962aae1603dbbe7e4304c
ThatGuyJohnForbes <thatbotjohn@gmail.com>
2021-04-24 19:16:47 +0100

    implement generate_totp() which returns a String wrapped in a result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::{convert::TryInto, time, thread};
 //use std::io::{Write, stdout};
 
 //use crossterm::{execute, ExecutableCommand, cursor};
@@ -36,10 +37,34 @@ fn hmac_sha1 (mut key: String, message: String) -> Result<String, ()> {
     Ok(ret.to_owned())
 }
 
+fn dynamic_truncation (hmac_string : String) -> Result<String, ()> {
+    //FIXME: do better error handling, such as a hexidecimal digit check
+    let offset : usize = hmac_string.chars().last().unwrap().to_digit(16).unwrap().try_into().unwrap();
+
+    let truncated = hmac_string.as_str()[offset*2..offset*2+8].to_owned();
+
+    let decimal = u32::from_str_radix(truncated.as_str(), 16).unwrap().to_string();
+
+    let decimal_offset = decimal.len();
+    let otp_code = decimal.as_str()[decimal_offset-6..decimal_offset].to_owned();
+
+    Ok(otp_code)
+}
+
+fn generate_totp () -> Result<String, ()> {
+    let seconds_since_epoch = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs();
+
+    let rounded_s_s_e = seconds_since_epoch - (seconds_since_epoch % 30);
+
+    let hash = hmac_sha1(SECRET_KEY.to_owned(), rounded_s_s_e.to_string()).unwrap();
+
+    let totp = dynamic_truncation(hash).unwrap();
+    Ok(totp)
+}
 
 
 #[test]
-fn sha1_test(){
+fn test_suite(){
     let precomputed_hash = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
 
     let hashed = sha1::Sha1::from("The quick brown fox jumps over the lazy dog").digest();
@@ -49,12 +74,24 @@ fn sha1_test(){
     let test_key = "WHDQ9I4W5FZSCCI0".to_owned();
     let test_message = "1397552400".to_owned();
 
-    let hmac_string = hmac_sha1(test_key,test_message);
+    let hmac_string = hmac_sha1(test_key,test_message).unwrap();
 
-    assert_eq!(hmac_string.unwrap(), "206bfb33934df4f39580897444fa0371776bf97a");
+    assert_eq!(hmac_string, "206bfb33934df4f39580897444fa0371776bf97a");
+
+    let otp_code = dynamic_truncation(hmac_string.clone()).unwrap();
+
+    assert_eq!(otp_code,"098426".to_owned());
 }
 
+const SECRET_KEY : &str = "InsecureSecret1234";
+
 fn main() -> Result<(), ()>{
+
+    for i in 0..173 {
+        println!("{}, {}", i, generate_totp().unwrap());
+        thread::sleep(time::Duration::from_millis(15000));
+    }
+
 
     Ok(())
 }
